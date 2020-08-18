ab7c598cf217696834cbbe39846327cf4a463676
Fatemeh Jahedpari <jahedpari@gmail.com>
2020-08-18 22:22:57 +0400

    Lesson 3 - PermMissingElem

---
+++ PermMissingElem.java
@@ -0,0 +1,57 @@
+/*
+
+An array A consisting of N different integers is given. The array contains integers in the range [1..(N + 1)], which means that exactly one element is missing.
+
+Your goal is to find that missing element.
+
+Write a function:
+
+class Solution { public int solution(int[] A); }
+
+that, given an array A, returns the value of the missing element.
+
+For example, given array A such that:
+
+  A[0] = 2
+  A[1] = 3
+  A[2] = 1
+  A[3] = 5
+the function should return 4, as it is the missing element.
+
+Write an efficient algorithm for the following assumptions:
+
+N is an integer within the range [0..100,000];
+the elements of A are all distinct;
+each element of array A is an integer within the range [1..(N + 1)].
+
+*/
+//*****	 Detected time complexity: O(N)
+//*****	 Correctness: 100%
+//*****	 Performance: 100%
+//*****	 Task Score:  100%
+
+import java.util.*;
+
+class Solution {
+    public int solution(int[] A) {
+       
+		if (A.length == 0)
+			return 1;
+
+		Arrays.sort(A);
+		int ans = A[A.length - 1] + 1;
+
+		for (int i = 0; i < A.length; i++) {
+
+			if (A[i] == i + 1)
+				continue;
+			else {
+				ans = i + 1;
+				break;
+			}
+
+		}
+
+		return ans;
+    }
+}
